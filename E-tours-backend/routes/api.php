9884b71c10d94da30d1f8f9335a27977178e2b62
badrbelabrik1 <badr.belabrik@gmail.com>
2026-09-08 23:17:28 +0100

    create admin middleware

--- E-tours-backend/routes/api.php
+++ E-tours-backend/routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\GameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/games', [GameController::class, 'index']);
+Route::get('/games/{game}', [GameController::class, 'show']);
+
 
 Route::middleware('auth:sanctum')->group(function () {
 
